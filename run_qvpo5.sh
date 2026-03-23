8e0bd831ebbc87a8f6762698992b0897088d5598
moqianyu <your_email@example.com>
2026-03-23 17:22:58 +0800

    测试最佳参数配置

---
+++ run_qvpo5.sh
@@ -0,0 +1,41 @@
+# 3/23 16:32
+# eav_penalty_coef 5     --chosen 2  batchsize 256
+# 降低critic rl
+python main.py \
+  --env_name Env \
+  --seed 42 \
+  --num_steps 400000 \
+  --batch_size 256 \
+  --gamma 0.99 \
+  --tau 0.005 \
+  --diffusion_lr 0.0001 \
+  --critic_lr 0.0003 \
+  --n_timesteps 20 \
+  --beta_schedule cosine \
+  --entropy_alpha 0.05 \
+  --train_sample 32 \
+  --behavior_sample 8 \
+  --target_sample 2 \
+  --eval_sample 16 \
+  --ac_grad_norm 1.0 \
+  --q_transform qadv \
+  --chosen 2 \
+  --q_neg 0.001 \
+  --cut 0.8 \
+  --policy_freq 2 \
+  --weighted \
+  --aug \
+  --normalize_state True \
+  --action_smooth_coef 0.1 \
+  --user_move_range 20.0 \
+  --reward_scale 0.1 \
+  --eav_threshold 10.0 \
+  --eav_penalty_coef 5 \
+  --eav_penalty_clip_max 200.0 \
+  --comm_threshold 10.0 \
+  --comm_penalty_coef 1.5 \
+  --comm_softplus_kappa 5.0 \
+  --comm_penalty_cap_per_user 15.0 \
+  --comm_penalty_cap_total 30.0 \
+  --start_steps 10000 \
+  --cuda cuda:3
